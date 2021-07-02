6e677b83fac69ff31b53dd75867418adfb371b5e
Caleb Anderson <calebtree0250@outlook.com>
2021-07-02 13:06:54 -0500

    added comments, added text to pause before sfc

--- virus_scan.bat
+++ virus_scan.bat
@@ -1,22 +1,27 @@
 @ECHO OFF
+REM Update defender signature
 call :ProgressMeter 0
 echo [32m---Defender Signature Update---[0m
 "%ProgramFiles%\WINDOW~1\MpCmdRun.exe" /signatureupdate
 call :ProgressMeter 10
 echo.
+
+REM Scan browser caches
 echo [32m---Scan Browser Caches---[0m
+REM Scan Edge
 IF EXIST %localappdata%\Microsoft\Windows\ (
 echo [36m--Edge--[0m
 "%ProgramFiles%\Windows Defender\MpCmdRun.exe" -Scan -ScanType 3 -File %localappdata%\Microsoft\Windows\
 call :ProgressMeter 20
 ) ELSE ECHO Edge not installed
-
+REM Scan Firefox
 echo.
 IF EXIST %localappdata%\Mozilla\ (
 echo [36m--FireFox--[0m
 "%ProgramFiles%\Windows Defender\MpCmdRun.exe" -Scan -ScanType 3 -File %localappdata%\Mozilla\
 call :ProgressMeter 30
 ) ELSE ECHO Firefox not installed
+REM Scan Chrome
 echo.
 IF EXIST %localappdata%\Google\ (
 echo [36m--Chrome--[0m
@@ -24,40 +29,50 @@ echo [36m--Chrome--[0m
 call :ProgressMeter 40
 ) ELSE ECHO Chrome not installed
 
+REM Scan this temp directory
 echo.
 echo [32m---Scan Temp---[0m
 "%ProgramFiles%\Windows Defender\MpCmdRun.exe" -Scan -ScanType 3 -File %localappdata%\Temp\
 call :ProgressMeter 60
 
+REM Scan email cache 
 echo.
 echo [32m---Scan email temp folders---[0m
+REM Scan Windows Mail app
 IF EXIST %localappdata%\Packages\microsoft.windowscommunicationsapps_8wekyb3d8bbwe\LocalState\Files\ (
 echo [36m--UWP Mail--[0m
 "%ProgramFiles%\Windows Defender\MpCmdRun.exe" -Scan -ScanType 3 -File %localappdata%\Packages\microsoft.windowscommunicationsapps_8wekyb3d8bbwe\LocalState\Files\
 call :ProgressMeter 70
 ) ELSE ECHO UWP Mail not installed
+REM Scan Outlook cache
 echo.
 IF EXIST %localappdata%\Microsoft\Windows\INetCache\Content.Outlook\ (
 echo [36m--Outlook--[0m
-:: alternate method using powershell
+:: alternate scan method using powershell
 powershell 'Start-MpScan -ScanPath "$env:LOCALAPPDATA\Microsoft\Windows\INetCache\Content.Outlook\" -ScanType CustomScan'
 call :ProgressMeter 80
 ) ELSE ECHO Outlook not installed.
 
+REM check image health
 echo.
 echo [32m---Image Health Checkup---[0m
 dism /online /cleanup-image /checkhealth
 echo.
 echo [32m---System File Check---[0m (CTRL^+C to quit)
 call :ProgressMeter 90
-PAUSE
+
+REM SFC
+ECHO Press any key to sfc . . . & PAUSE >NUL
 sfc /scannow
 call :ProgressMeter 100
 echo.
+REM todo: if checkhealth is bad continue with cleanup-image /restorehealth
 REM dism /online /cleanup-image /startcomponentcleanup
-echo.
+REM end
 PAUSE
-exit /b
+EXIT /B
+
+:: ========== FUNCTIONS ==========
 :ProgressMeter
 SETLOCAL ENABLEDELAYEDEXPANSION
 SET ProgressPercent=%1
