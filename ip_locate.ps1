7424fcdee687b93c4a237c6e2a3a4b17fef19587
calebTree <ande4905@gmail.com>
2022-08-26 08:29:20 -0500

    included public ip addr

--- ip_locate.ps1
+++ ip_locate.ps1
@@ -1,10 +1,16 @@
-Write-Host "ipinfo.io`n"
+$myIp = (Invoke-WebRequest ifconfig.me/ip).Content.Trim()
+Write-Host "ifconfig.me/ip:" $myIp
+Write-Host "ipinfo.io:"
 $id = 0
 while($ipAddr -ne 'q') {
-	$ipAddr = Read-Host -Prompt "Enter an ip address ('q' to quit)"
+	$ipAddr = Read-Host -Prompt "Enter an ip address ('q' to quit, 'myip' to return public ip info)"
 	if($ipAddr -ne 'q') {
 		$id++
 		Invoke-RestMethod -Uri ('http://ipinfo.io/'+("$ipAddr"))
+		Write-Host "-------------------$id--------------------`n"
+	} elseif ($ipAddr -eq 'myip') {
+		$id++
+		Invoke-RestMethod -Uri ('https://ipinfo.io/what-is-my-ip')
 		Write-Host "-------------------$id--------------------`n"
 	}
 }
