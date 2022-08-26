e59c8ca4a52561beae16153be45ebca3f9930b12
calebTree <ande4905@gmail.com>
2022-08-26 09:41:54 -0500

    improved ip_locate output

--- ip_locate.ps1
+++ ip_locate.ps1
@@ -1,16 +1,16 @@
-$myIp = (Invoke-WebRequest ifconfig.me/ip).Content.Trim()
-Write-Host "ifconfig.me/ip:" $myIp
+Write-Host "ifconfig.me/ip:" (Invoke-RestMethod -Uri "ifconfig.me/ip")
 Write-Host "ipinfo.io:"
 $id = 0
 while($ipAddr -ne 'q') {
 	$ipAddr = Read-Host -Prompt "Enter an ip address ('q' to quit, 'myip' to return public ip info)"
 	if($ipAddr -ne 'q') {
 		$id++
-		Invoke-RestMethod -Uri ('http://ipinfo.io/'+("$ipAddr"))
-		Write-Host "-------------------$id--------------------`n"
-	} elseif ($ipAddr -eq 'myip') {
-		$id++
-		Invoke-RestMethod -Uri ('https://ipinfo.io/what-is-my-ip')
-		Write-Host "-------------------$id--------------------`n"
+		Write-Host "-------------------$id--------------------"
+		if ($ipAddr -eq 'myip') {
+			((Invoke-RestMethod -Uri "https://ipinfo.io/what-is-my-ip") | Out-String).Trim()
+		} else {
+			((Invoke-RestMethod -Uri "http://ipinfo.io/$ipAddr") | Out-String).Trim()
+		}
+		Write-Host "-------------------$("-" * $id.tostring().length)--------------------"
 	}
 }
